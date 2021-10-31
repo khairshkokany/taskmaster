a479c215ab11f3c49d3f4ce3febc2dee39d289ee
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-10-31 20:35:40 +0200

    finished with read me

---
+++ app/src/main/java/com/example/taskmaster/AllTask.java
@@ -0,0 +1,28 @@
+package com.example.taskmaster;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class AllTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_task);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(AllTask.this, MyTask.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
